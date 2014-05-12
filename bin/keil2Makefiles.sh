f55af17a196fe61ffb06a60f53f10e70bf8364cf
tim <tdwebste@mintcontiki>
2014-05-11 17:28:38 -0700

    fix up keil to Makefile converter
    Takes a *.uvproj file and outputs a Makefile
    The Makefile requires only a little hand editing

--- bin/keil2Makefiles.sh
+++ bin/keil2Makefiles.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
+uvprojfile="$1"
+
+echo '
+# check the TARGET_CHIP and BOARD
+TARGET_CHIP := NRF51822_QFAA_CA
+BOARD := BOARD_PCA10001
+'
+
+echo '
+# check CFAGS remove extra -DNRF51 -DDEBUG_NRF_USER
+CFLAGS += -DNRF51822_QFAA_CA'
+grep '<Define>'  $uvprojfile | sort -u \
+	| sed -e 's#<Define>##' \
+	-e 's#</Define>##' \
+	| grep -v '^[ 	]*$' \
+	| sed -e 's#  *# -D#g' \
+	-e 's#^[ 	]*#CFLAGS += #'
+
+
+echo ''
 echo 'C_SOURCE_FILES += main.c'
-grep '<FilePath>'  *.uvproj | sort -u \
+grep '<FilePath>'  $uvprojfile | sort -u \
 	| grep -v 'arm_startup_nrf51.s' \
 	| grep -v 'main.c' \
 	| grep -v 'system_nrf51.c' \
@@ -11,7 +31,7 @@ grep '<FilePath>'  *.uvproj | sort -u \
 	| tr '\' '/' 2>/dev/null
 
 echo ''
-grep '<IncludePath>'  *.uvproj \
+grep '<IncludePath>' $uvprojfile  \
 	| sed -e 's#<IncludePath>##' \
 	-e 's#</IncludePath>##' \
 	| grep -v '^[ 	]*$' \
@@ -23,4 +43,32 @@ grep '<IncludePath>'  *.uvproj \
 	-e 's#[ 	]*$#"#' \
 	-e 's#^[ 	]*#INCLUDEPATHS += #' \
 	| tr '\' '/' 2>/dev/null
+
+echo ''
+#grep '<TargetName>' $uvprojfile  \
+#	| sed -e 's#<TargetName>##' \
+#	-e 's#</TargetName>##' \
+#	| grep -v '^[ 	]*$' \
+#	| head -1 \
+#	| sed -e 's#^[ 	]*##' \
+#	-e 's#[ 	].*##' \
+#	-e 's#^#OUTPUT_FILENAME := #' \
+#	-e 's#$#_gcc#'
+
+echo "$uvprojfile" | sed -e 's#\.uvproj#_gcc#' \
+	-e 's#^#OUTPUT_FILENAME := #' 
+
+echo '
+SDK_PATH = ../../../../../
+
+DEVICE_VARIANT := xxaa
+#DEVICE_VARIANT := xxab
+
+USE_SOFTDEVICE := S110
+#USE_SOFTDEVICE := s210
+
+include $(SDK_PATH)Source/templates/gcc/Makefile.common
+'
+
+
 
